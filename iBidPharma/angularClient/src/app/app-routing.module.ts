72e152457ee0469428ead2830d5aa9392c3e57d2
Shripad-VK <shripad.kshirsagar18@gmail.com>
2021-01-14 15:54:32 +0530

    Adding script file and empty angular project

--- iBidPharma/angularClient/src/app/app-routing.module.ts
+++ iBidPharma/angularClient/src/app/app-routing.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CreateEmployeeComponent } from './create-employee/create-employee.component';
-import { Employee } from './employee';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 
-const routes: Routes = [
-  {path:'employees', component : EmployeeListComponent}, 
-  {path:'add', component :CreateEmployeeComponent},
-  {path:'update/:id',component : UpdateEmployeeComponent} 
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

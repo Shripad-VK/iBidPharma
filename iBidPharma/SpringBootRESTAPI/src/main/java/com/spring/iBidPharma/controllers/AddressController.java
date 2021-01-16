9bdea8d5ac40bb25f47bae71e599bd2a369d6fe9
Shripad-VK <shripad.kshirsagar18@gmail.com>
2021-01-16 06:57:06 +0530

    Modifying AddressController changing correcting pincode into pin_code

--- iBidPharma/SpringBootRESTAPI/src/main/java/com/spring/iBidPharma/controllers/AddressController.java
+++ iBidPharma/SpringBootRESTAPI/src/main/java/com/spring/iBidPharma/controllers/AddressController.java
@@ -37,7 +37,7 @@ public class AddressController {
 	public Address createAddress( @RequestBody Address address)
 	{
 		
-		System.out.println(address.getPincode());
+		System.out.println(address.getPin_code());
 		return addressRepositroy.save(address);
 		
 	}
@@ -58,7 +58,7 @@ public class AddressController {
 		address.setLine1(info.getLine1());
 		address.setLine2(info.getLine2());
 		
-		address.setPincode(info.getPincode());
+		address.setPin_code(info.getPin_code());
 		address.setState(info.getState());
 		
 		return addressRepositroy.save(address);

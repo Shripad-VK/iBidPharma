ff3395e20fd4af3c67ab8207472f3a4acdceb09f
rajnandinimali <rajnandinimali6@gmail.com>
2021-01-17 20:28:39 +0530

    Admin Nav Bar

--- iBidPharma/angularClient/src/app/Admin/admin-home/admin-home.component.ts
+++ iBidPharma/angularClient/src/app/Admin/admin-home/admin-home.component.ts
@@ -10,6 +10,15 @@ export class AdminHomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("sidebar").style.left = "0";
+      } else {
+        document.getElementById("sidebar").style.left = "-50px";
+      }
+      prevScrollpos = currentScrollPos;
   }
-
+  }
 }

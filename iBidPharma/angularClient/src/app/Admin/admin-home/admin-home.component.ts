7fbd4452736bde0a6c89ac8377661a53ebfc8c22
rajnandinimali <rajnandinimali6@gmail.com>
2021-01-18 15:56:00 +0530

    product form

--- iBidPharma/angularClient/src/app/Admin/admin-home/admin-home.component.ts
+++ iBidPharma/angularClient/src/app/Admin/admin-home/admin-home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery'
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
@@ -10,15 +10,8 @@ export class AdminHomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-      var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.getElementById("sidebar").style.left = "0";
-      } else {
-        document.getElementById("sidebar").style.left = "-50px";
-      }
-      prevScrollpos = currentScrollPos;
-  }
-  }
+   
+   
+  
+}
 }

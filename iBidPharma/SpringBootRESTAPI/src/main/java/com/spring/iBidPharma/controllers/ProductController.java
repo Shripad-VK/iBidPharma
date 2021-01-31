e7fa6e1aafae3ea2e5c7ce3fbf3b58721c6b13c6
Shripad-VK <shripad.kshirsagar18@gmail.com>
2021-01-31 07:24:01 +0530

    Show Images

--- iBidPharma/SpringBootRESTAPI/src/main/java/com/spring/iBidPharma/controllers/ProductController.java
+++ iBidPharma/SpringBootRESTAPI/src/main/java/com/spring/iBidPharma/controllers/ProductController.java
@@ -1,9 +1,17 @@
 package com.spring.iBidPharma.controllers;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -93,7 +102,18 @@ public class ProductController {
 	}
 	
 
-
+	@GetMapping("products/image")
+	@ResponseBody
+	public ResponseEntity<Resource> getProductImg( @RequestParam("name") String name)
+			throws IOException {
+    	Path root = Paths.get("src/main/resources/static/images/");
+		Path f = root.resolve(name + ".jpg");
+		Resource resource = new UrlResource(f.toUri());
+		Resource file = resource;
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\"" + ((UrlResource) file).getFilename() + "\"")
+				.contentType(MediaType.IMAGE_JPEG).body(file);
+	}
 
 	
 	
